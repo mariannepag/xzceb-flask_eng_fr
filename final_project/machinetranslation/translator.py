8cc5dfb4d21e22487ad257c52a08725d8da2e79b
Marianne Page <mariannepage@hotmail.ca>
2023-07-15 15:42:41 -0400

    modified unit test

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,10 +1,10 @@
 from deep_translator import MyMemoryTranslator
 
 def englishToFrench(englishText):
-    frenchText=MyMemoryTranslator(source='EN', target='FR').translate(englishText)
+    frenchText=MyMemoryTranslator(source='en-CA', target='fr-CA').translate(englishText)
     return frenchText
 
 def frenchToEnglish(frenchText):
-    englishText=MyMemoryTranslator(source='FR', target='EN').translate(frenchText)
+    englishText=MyMemoryTranslator(source='fr-CA', target='en-CA').translate(frenchText)
     return englishText
 
