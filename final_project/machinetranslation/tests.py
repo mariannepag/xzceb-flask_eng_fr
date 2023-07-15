22eba0c35228950c44f94b770fd902c2ec8aa0f7
Marianne Page <mariannepage@hotmail.ca>
2023-07-15 16:12:18 -0400

    added modifications

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from translator import english_to_french, french_to_english
+from machinetranslation.translator import english_to_french, french_to_english
 
 class TestTranslations(unittest.TestCase):
     def test_e2f(self):
