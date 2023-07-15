ca11f3976ae9c92750a59556e900e6ea958eadf3
Marianne Page <mariannepage@hotmail.ca>
2023-07-15 15:50:26 -0400

    added docstring, conformed to snake_case

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,13 +1,13 @@
 import unittest
-from translator import englishToFrench, frenchToEnglish
+from translator import english_to_french, french_to_english
 
 class TestTranslations(unittest.TestCase):
     def test_e2f(self):
-        self.assertEqual(englishToFrench('Hello'),'Bonjour')
-        self.assertEqual(englishToFrench('Book'),'Livre')
+        self.assertEqual(english_to_french('Hello'),'Bonjour')
+        self.assertEqual(english_to_french('Book'),'Livre')
 
     def test_f2e(self):
-        self.assertEqual(frenchToEnglish('Bonjour'),'Hello')
-        self.assertEqual(frenchToEnglish('Rouge'),'Red')
+        self.assertEqual(french_to_english('Bonjour'),'Hello')
+        self.assertEqual(french_to_english('Rouge'),'Red')
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,10 +1,12 @@
+"""import translator"""
 from deep_translator import MyMemoryTranslator
 
-def englishToFrench(englishText):
-    frenchText=MyMemoryTranslator(source='en-CA', target='fr-CA').translate(englishText)
-    return frenchText
+def english_to_french(english_text):
+    """function to translate from english to french"""
+    french_text=MyMemoryTranslator(source='en-CA', target='fr-CA').translate(english_text)
+    return french_text
 
-def frenchToEnglish(frenchText):
-    englishText=MyMemoryTranslator(source='fr-CA', target='en-CA').translate(frenchText)
-    return englishText
-
+def french_to_english(french_text):
+    """function to translate from french to english"""
+    english_text=MyMemoryTranslator(source='fr-CA', target='en-CA').translate(french_text)
+    return english_text
